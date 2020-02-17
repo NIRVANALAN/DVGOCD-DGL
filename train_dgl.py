9675e20a5f448c7e2857829d3d63b7401a544e73
NIRVANALAN <maverickers@outlook.com>
2020-02-17 03:32:46 +0000

    update

--- train_dgl.py
+++ train_dgl.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 import networkx as nx
 import numpy as np
 import scipy.sparse as sp
+import sklearn
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -22,7 +23,6 @@ from nocd.nn import BerpoDecoder
 def evaluate(model_saver, model, features, Z_gt, thresh):
     model_saver.restore()
     model.eval()
-    # logits = F.relu(model(x_norm, adj_norm))
     logits = F.relu(model(features))
     preds = logits.cpu().detach().numpy() > thresh
     nmi = nocd.metrics.overlapping_nmi(preds, Z_gt)
@@ -38,24 +38,54 @@ def main(args):
     balance_loss = True     # whether to use balanced loss
     stochastic_loss = True  # whether to use stochastic or full-batch training
     batch_size = 20000      # batch size (only for stochastic training)
+
+
+    multitask_data = set(['ppi'])
+    multitask = args.dataset in multitask_data
+
     # load and preprocess dataset
-    # data = load_data(args)
-    loader = nocd.data.load_dataset('data/mag_cs.npz')
-    A, features, Z_gt = loader['A'], loader['X'], loader['Z']
-    n_nodes, n_classes = Z_gt.shape
-    graph = nx.Graph(A)
-    feature_norm = normalize(features)
-    features = torch.FloatTensor(feature_norm.todense())
-    labels = torch.LongTensor(Z_gt)
+    if 'npz' in args.dataset:
+        loader = nocd.data.load_dataset('data/mag_cs.npz')
+        A, features, Z_gt = loader['A'], loader['X'], loader['Z']
+        n_nodes, n_classes = Z_gt.shape
+        graph = nx.Graph(A)
+        feature_norm = normalize(features)
+        features = torch.FloatTensor(feature_norm.todense())
+        labels = torch.LongTensor(Z_gt)
+        # x_norm = nocd.utils.to_sparse_tensor(feature_norm).cuda()
+    else:
+        data = load_data(args)
+        train_nid = np.nonzero(data.train_mask)[0].astype(np.int64)
+        train_feats = data.features[train_nid]
+        scaler = sklearn.preprocessing.StandardScaler()
+        scaler.fit(train_feats)
+        features = scaler.transform(data.features)
+        features = torch.FloatTensor(features)
+        n_classes = data.num_labels
+        if not multitask:
+            labels = torch.LongTensor(data.labels)
+        else:
+            labels = torch.FloatTensor(data.labels)
+        n_nodes = labels.shape[0]
+        A = data.graph.adjacency_matrix(transpose=False)
+        graph = data.graph
+        pdb.set_trace()
+
+    # add self loop
+    if args.self_loop:
+        graph.remove_edges_from(nx.selfloop_edges(graph))
+        graph.add_edges_from(zip(graph.nodes(), graph.nodes()))
+        print("adding self-loop edges")
+
+    g = DGLGraph(graph, readonly=True)
+    n_edges = g.number_of_edges()
     in_feats = features.shape[1]
-    n_edges = graph.number_of_edges()
-    x_norm = nocd.utils.to_sparse_tensor(feature_norm).cuda()
-    # gnn = nocd.nn.ImprovedGCN(x_norm.shape[1], hidden_sizes, n_classes).cuda()
-    # adj_norm = gnn.normalize_adj(A)
+
     print("""----Data statistics------'
-      #Edges %d
-      #Classes %d """
-      %(n_edges, n_classes))
+    #Edges %d
+    #Classes %d """
+    %(n_edges, n_classes))
+
 
     if args.gpu < 0:
         cuda = False
@@ -66,16 +96,9 @@ def main(args):
         labels = labels.cuda()
 
     # graph preprocess and calculate normalization factor
-    # add self loop
-    if args.self_loop:
-        graph.remove_edges_from(nx.selfloop_edges(graph))
-        graph.add_edges_from(zip(graph.nodes(), graph.nodes()))
-    g = DGLGraph(graph)
-    n_edges = g.number_of_edges()
 
     # create GAT model
     heads = ([args.num_heads] * args.num_layers) + [args.num_out_heads]
-    # model = create_model(name=args.arch,)
     model = create_model(args.arch,g,
                 num_layers=args.num_layers,
                 in_dim=in_feats,
@@ -88,15 +111,6 @@ def main(args):
                 negative_slope=args.negative_slope,
                 residual=args.residual)
 
-    # create GCN model
-    # model = GCN(g,
-    #             in_feats,
-    #             args.n_hidden,
-    #             n_classes,
-    #             args.n_layers,
-    #             F.relu,
-    #             args.dropout,
-    #             batch_norm=True)
 
     if cuda:
         model.cuda()
@@ -104,7 +118,6 @@ def main(args):
     sampler = nocd.sampler.get_edge_sampler(A, batch_size, batch_size, num_workers=5)
     decoder = nocd.nn.BerpoDecoder(n_nodes, A.nnz, balance_loss=balance_loss) # ? nnz: number of nonzero values
     # use optimizer
-    # model = gnn
     # optimizer = torch.optim.SGD(model.parameters(), lr = args.lr)
     optimizer = torch.optim.Adam(model.parameters(),
                                  lr=args.lr,)
@@ -123,7 +136,6 @@ def main(args):
             with torch.no_grad():
                 model.eval()
                 # Compute validation loss
-                # logits = F.relu(model(x_norm, adj_norm))
                 logits = F.relu(model(features))
                 val_loss = decoder.loss_full(logits, A)
                 
@@ -139,7 +151,6 @@ def main(args):
         # Training step
         model.train()
         optimizer.zero_grad()
-        # logits = F.relu(model(x_norm, adj_norm))
         logits = F.relu(model(features))
         ones_idx, zeros_idx = batch
         if stochastic_loss:
